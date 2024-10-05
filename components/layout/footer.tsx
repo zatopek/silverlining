06a2da1aad1bb3c1ca9661ea7ccd2641da25a01d
Gautam Velpula <gvelpula@focusbrands.com>
2024-10-05 13:34:35 +0530

    Removed shopping component

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,4 +1,3 @@
-
 import FooterMenu from 'components/layout/footer-menu';
 import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
@@ -10,12 +9,13 @@ export default async function Footer() {
   const currentYear = new Date().getFullYear();
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
-  const menu = await getMenu('next-js-frontend-footer-menu');
+  // const menu = await getMenu('next-js-frontend-footer-menu');
+  const menu = {};
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
 
   return (
     <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm dark:border-neutral-700 md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0">
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
         <Suspense
           fallback={
             <div className="flex h-[188px] w-[200px] flex-col gap-2">
